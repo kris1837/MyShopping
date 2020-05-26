429f3dbfd8d6b2778424d5451e5bcc0f0856f74a
Amirkhan <amirkhan13101998@gmail.com>
2020-05-26 20:56:55 +0600

    minor changes.

--- app/src/main/java/kz/shag/myshopping/ProductAdapter.java
+++ app/src/main/java/kz/shag/myshopping/ProductAdapter.java
@@ -1,4 +1,55 @@
 package kz.shag.myshopping;
 
-public class ProductAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHolder> {
+
+    Context context;
+
+    public ProductAdapter(Context ct,)
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.row,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.priceTextView.setText();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView nameTextView,priceTextView;
+        Button buyButton;
+        ImageView productImageView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameTextView = itemView.findViewById(R.id.textViewName);
+            priceTextView = itemView.findViewById(R.id.textViewPrice);
+            buyButton = itemView.findViewById(R.id.buttonBuy);
+            productImageView = itemView.findViewById(R.id.imageViewProduct);
+
+
+        }
+    }
 }
