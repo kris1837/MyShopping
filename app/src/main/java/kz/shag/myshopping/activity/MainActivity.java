8323b19cf777e6903bd927372f98f2a12011df2d
Kirill <kris1837@gmail.com>
2020-05-26 22:11:30 +0600

    Merge resolve

--- app/src/main/java/kz/shag/myshopping/activity/MainActivity.java
+++ app/src/main/java/kz/shag/myshopping/activity/MainActivity.java
@@ -6,9 +6,18 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import kz.shag.myshopping.ProductAdapter;
 import kz.shag.myshopping.R;
+import kz.shag.myshopping.entity.Product;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +25,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = findViewById(R.id.recyclerView);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Товары");
         setSupportActionBar(toolbar);
 
+        //data from Firebase
+        List<Product> products = new ArrayList<Product>();
+        //
 
+        ProductAdapter productAdapter = new ProductAdapter(this, products);
+        recyclerView.setAdapter(productAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         //getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         //getSupportActionBar().setCustomView(R.layout.activity_main);
