3231bbc548fffc2aa69fa435ebb9980b63803017
Yernar Yergaliyev <yernar8m@gmail.com>
2020-05-26 21:53:19 +0600

    update cart activity

--- app/src/main/java/kz/shag/myshopping/CartActivity.java
+++ app/src/main/java/kz/shag/myshopping/CartActivity.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
+import kz.shag.myshopping.entity.Product;
+import kz.shag.myshopping.localDB.LocalDataBase;
+
 public class CartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //load data from local db
-
+        List<Product> products = LocalDataBase.getInstance(this).productDao().getAll();
 
         //add to recycler view - Alibi
     }
