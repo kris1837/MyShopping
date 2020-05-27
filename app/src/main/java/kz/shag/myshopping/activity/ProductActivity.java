65d6f0568ef0e7c4c8d2db59eaa22de6f9cab26b
JeLion <djoniqaz@gmail.com>
2020-05-27 21:27:48 +0600

    Add ProductActivity

---
+++ app/src/main/java/kz/shag/myshopping/activity/ProductActivity.java
@@ -0,0 +1,16 @@
+package kz.shag.myshopping.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import kz.shag.myshopping.R;
+
+public class ProductActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+    }
+}
