9bee0ecbc3107cfd7528d49a2fb4760e5d537c72
Yernar Yergaliyev <yernar8m@gmail.com>
2020-06-01 22:00:16 +0600

    update initializer

--- app/src/main/java/kz/shag/myshopping/localDB/Initializer.java
+++ app/src/main/java/kz/shag/myshopping/localDB/Initializer.java
@@ -8,11 +8,11 @@ import kz.shag.myshopping.entity.Product;
 public class Initializer {
     public static void Init(ProductRepository productRepository){
 
-        Product p1 = new Product("Banana", "Yellow fruit",
+        Product p1 = new Product(1, "Banana", "Yellow fruit",
                 1.5, "https://upload.wikimedia.org/wikipedia/commons/8/8a/Banana-Single.jpg", 1);
-        Product p2 = new Product("Potato", "vegetable",
+        Product p2 = new Product(2, "Potato", "vegetable",
                 1.1, "", 1);
-        Product p3 = new Product("Apple", "fruit",
+        Product p3 = new Product(3, "Apple", "fruit",
                 1.3, "", 1);
 
         productRepository.insertProduct(p1);
